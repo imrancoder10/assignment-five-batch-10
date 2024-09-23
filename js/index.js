7ac7038cf2a7dad67ac9f998a29419f1a65ce77b
imrancoder10 <imranhossain1148@gmail.com>
2024-09-23 19:43:13 +0600

    modal set successfully

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-// const blogBtn = document.getElementsByClassName('blog-btn');
-// blogBtn.addEventListener('click', function() {
-//     alert('Button was clicked!');
-// });
-
-
+// noakhali calculator starts here......................
 document.getElementById('donate-btn')
-        .addEventListener('click', function (event) {
-            event.preventDefault();
-            const noakhaliDonation = inputFieldValue('input-noakhali-donation');
-            if(!isNaN(noakhaliDonation) && noakhaliDonation > 0 ){
-                const noakhaliBalace = textValue('noakhali-blance');
-                const topDecreaseBlance = textValue('top-blance');
-                const decreaseTotalBlance = topDecreaseBlance - noakhaliDonation;
-                const addBlanceToNoakhali = noakhaliBalace + noakhaliDonation;
-                injectBlance('top-blance', decreaseTotalBlance);
-                injectBlance('noakhali-blance', addBlanceToNoakhali);
-            }
-            else{
-                alert('Wrong Input!');
-                return;
-            }
-        });
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const noakhaliDonation = inputFieldValue('input-noakhali-donation');
+        if (!isNaN(noakhaliDonation) && noakhaliDonation > 0) {
+            const noakhaliBalace = textValue('noakhali-blance');
+            const topDecreaseBlance = textValue('top-blance');
+            const decreaseTotalBlance = topDecreaseBlance - noakhaliDonation;
+            const addBlanceToNoakhali = noakhaliBalace + noakhaliDonation;
+            injectBlance('top-blance', decreaseTotalBlance);
+            injectBlance('noakhali-blance', addBlanceToNoakhali);
+            // ===================================================
+            const modal = document.getElementById('my_modal_5');
+            modal.showModal();
+            // ===================================================
+        } else {
+            alert('Wrong Input!');
+            return;
+        }
+    });
+
+// noakhali calculator ends here......................
